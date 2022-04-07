b727ca3d6b0ed2e71acf924ac96bc39af491d069
JackBecknell <52moosefit@gmail.com>
2022-04-07 14:45:51 -0400

    Created a new file 'serializers' and set up some groundwork for the future + Also Set groundwork inside of views.py making the outline for all funtions to come + Added likes as a new field for the songs model.

---
+++ songs/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        pass
+
+    
--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from .models import Song
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
-# Create your views here.
+
+class SongList(APIView):
+    def get(self, request, format=None):
+        pass
+
+    def post(self, request, format=None):
+        pass
+
+class SongDetail(APIView):
+    #helper function
+    def get_object(self,pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        pass
+
+    def put(self, request, pk, format=None):
+        pass
+
+    def delete(self, request, pk, format=None):
+        pass
+
+    
+
